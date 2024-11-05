6f828c2f7cdf923efd294ac9125ada08b4241b5e
Ítalo <igranjo@gmail.com>
2024-11-05 16:00:12 -0300

    Inicio do projeto de aprendizado React

---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+import './Header.css'
+
+function Header() {
+  return (
+    <>
+      <nav>
+        <div className="section">
+          <section>
+            <ul className="itens">
+              <li className="item">
+                <a href="#Eu">Quem sou eu?</a></li>
+              <li className="item"> <a href="#Projetos">Projetos</a></li>
+            </ul>
+          </section>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Header
